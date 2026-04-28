0d6ac73a7bacf1fe4738e1d80546ab94038c17ca
Manuel Sá <manuel.sa@wisecrop.com>
2026-04-28 17:31:49 +0100

    Updates

--- assets/dist-BBlFrNsD.js
+++
@@ -1,1 +0,0 @@
-import{n as e,r as t,t as n}from"./index-BFwH2a5W.js";var r={renderer:t,...e,...n};export{r as default};
---
+++ assets/dist-IOudbksI.js
@@ -0,0 +1,1 @@
+import{n as e,r as t,t as n}from"./index-BDUiCZPa.js";var r={renderer:t,...e,...n};export{r as default};
